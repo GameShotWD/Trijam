a2b0607c61edb05ace2a6866d11bbef1d58e5a82
setagiSan <118693438+SetagiSan@users.noreply.github.com>
2024-10-05 17:05:48 +0300

    ShootUpdate

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    [SerializeField] private GameObject Shot;
+    [SerializeField] private float shot_delay = 0;
+    private bool delay = false;
+    public bool active = false;
+    [SerializeField] private float ShotPower = 0;
+    IEnumerator Wait()
+    {
+        delay = true;
+        yield return new WaitForSeconds(shot_delay);
+        delay = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!delay)
+        {
+            if (active)
+            {
+                GameObject Ammo = Instantiate(Shot, transform.position + transform.forward, this.transform.rotation);
+                Ammo.AddComponent<Rigidbody>().AddForce(transform.forward * ShotPower, ForceMode.Impulse);
+                Destroy(Ammo, 5);
+                StartCoroutine(Wait());
+            }
+        }
+    }
+}
