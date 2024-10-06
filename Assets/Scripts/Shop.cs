36fd7cd67f34bf762f32de49ead5491e1f769720
setagiSan <118693438+SetagiSan@users.noreply.github.com>
2024-10-06 12:50:44 +0300

    ShopUpdate

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Shop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject Canvas;
+    private void Start()
     {
-        
+        Canvas = GameObject.Find("Shop");
+        Canvas.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M)) {
+            ChangeState();
+        }
+    }
+    private void ChangeState()
+    {
+        if (Canvas.activeSelf == true)
+        {
+            Canvas.SetActive(false);
+        }
+        else
+        {
+            Canvas.SetActive(true);
+        }
     }
 }
